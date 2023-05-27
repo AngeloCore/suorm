bb42d7f502459fffdbb4d73701525657ec597571
AngeloCore <angelocoree@gmail.com>
2023-05-27 18:51:55 +0300

    Add lib and types to .gitignore

--- types/types.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const fieldDataTypes: readonly ["any", "array", "bool", "datetime", "decimal", "duration", "float", "int", "number", "object", "string", "record"];
-export type FieldDataTypes = (typeof fieldDataTypes)[number];
-export declare const defaultValues: readonly ["NONE", "NULL", "TRUE", "FALSE"];
-export type DefaultValues = (typeof defaultValues)[number];
-export type Value = DefaultValues | Lowercase<DefaultValues> | number | (string & {});
